5b6913f8bc715a7265630203bd5b166dcfe5104e
Behnam <behnam.sep@gmail.com>
2025-04-19 15:17:32 +0200

    init the project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+</script>
+<template>
+  <Navbar />
+  <Hero title="Behnam" subtitle="Best frontend developer in this world!"/>
+  <p class="text-xl">dfdfd</p>
+</template>
